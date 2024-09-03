d27fb098674b0bd63a91bd839565b25d3bbd1aa6
Oleg Trofimov <oleg.trofimov2@aptiv.com>
2024-09-03 15:59:35 +0300

    add asan_options

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -2,4 +2,5 @@
 set -e
 cd /altv
 node /root/setup/server.js
+export ASAN_OPTIONS=halt_on_error=0:handle_abort=1:exitcode=0:verbosity=0:detect_leaks=1:detect_odr_violation=1:alloc_dealloc_mismatch=1:log_path=/altv/asan/asan.log
 ./altv-server "$@"
