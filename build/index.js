9bb9c44ce652cecd4a9a7dd0d89b22885a10d5e4
Till Schreiber <till@tillschreiber.de>
2024-10-03 19:14:49 +0200

    bump dotnet and node version

--- build/index.js
+++ build/index.js
@@ -110,7 +110,10 @@ async function buildBranch(branch, moduleType) {
     {
         const [buildHash, tags] = generateTags(branch, version, moduleType, modulesVersions);
         console.log(chalk.gray('Building server with tags ' + tags.map(e => chalk.white(chalk.bold(e))).join(', ')));
-        await buildDocker(serverImageName, tags, branch, './server/Dockerfile', moduleType, buildHash);
+
+        const dockerFile = branch === 'release' ? './server/Dockerfile' : './server/DockerfileDotnet8'
+
+        await buildDocker(serverImageName, tags, branch, dockerFile, moduleType, buildHash);
         console.log(chalk.green('Server on branch ') + chalk.white(chalk.bold(branch)) + chalk.green(' built successfully'));
     }
 
